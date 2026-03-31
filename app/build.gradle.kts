7ab336df79b7fccec6687d38deec0e2287dafbcc
Joe Smith <004jsmith@gmail.com>
2026-03-31 08:20:36 +0100

    build: configure Android client APP build.gradle generation for protobuf and gRPC code
    - added protobuf Gradle plugin
    - enabled lite protobuf and gRPC Java generation
    - configured app module to use shared proto directory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
+import com.google.protobuf.gradle.proto
+
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.protobuf") version "0.9.6"
 }
 
 android {
     namespace = "com.karstonn.alarm"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.alarm"
+        applicationId = "com.karstonn.alarm"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -27,9 +28,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    sourceSets {
+        getByName("main") {
+            proto {
+                srcDir("../proto")
+            }
+        }
     }
 }
 
@@ -38,7 +48,41 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("io.grpc:grpc-okhttp:1.76.0")
+    implementation("io.grpc:grpc-protobuf-lite:1.76.0")
+    implementation("io.grpc:grpc-stub:1.76.0")
+    implementation("com.google.protobuf:protobuf-javalite:4.31.1")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:4.31.1"
+    }
+
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.76.0"
+        }
+    }
+
+    generateProtoTasks {
+        all().configureEach {
+            builtins {
+                create("java") {
+                    option("lite")
+                }
+            }
+            plugins {
+                create("grpc") {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
