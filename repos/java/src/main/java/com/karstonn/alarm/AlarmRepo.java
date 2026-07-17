ea206c7011c7efc810c2ba76a401a497c971de74
Joe Smith <004jsmith@gmail.com>
2026-07-17 04:05:30 +0100

    Add(repo service): Changed List alarm service to return only identifying information rather than complete alarm repo

--- repos/java/src/main/java/com/karstonn/alarm/AlarmRepo.java
+++ repos/java/src/main/java/com/karstonn/alarm/AlarmRepo.java
@@ -1,18 +1,20 @@
-package com.karstonn.alarm.Repo;
+package com.karstonn.alarm;
 
 import com.karstonn.alarmsystem.proto.Alarm;
 import com.karstonn.alarmsystem.proto.AlarmId;
+import com.karstonn.alarmsystem.proto.AlarmListResponse;
 import com.karstonn.alarmsystem.proto.AlarmRequestResponse;
+import com.karstonn.alarmsystem.proto.UpdateAlarmRequest;
 
 import java.util.List;
 public interface AlarmRepo {
     Alarm getAlarm(AlarmId id);
 
-    List<Alarm> listAlarms();
+    AlarmListResponse listAlarms();
 
     AlarmRequestResponse addAlarm(Alarm alarm);
 
-    AlarmRequestResponse updateAlarm(AlarmId id, Alarm alarm);
+    AlarmRequestResponse updateAlarm(UpdateAlarmRequest updateRequest);
 
     AlarmRequestResponse removeAlarm(AlarmId id);
 }
