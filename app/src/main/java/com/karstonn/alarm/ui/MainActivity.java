ce80d7364a871e64a10ca0cc49c0c581a7af72c3
Joe Smith <004jsmith@gmail.com>
2026-07-20 13:01:00 +0100

    Style(UI): UI no longer covered by floating bottom bars (e.g. samsung)

--- app/src/main/java/com/karstonn/alarm/ui/MainActivity.java
+++ app/src/main/java/com/karstonn/alarm/ui/MainActivity.java
@@ -1,22 +1,47 @@
 package com.karstonn.alarm.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.karstonn.alarm.R;
 import com.karstonn.alarm.ui.scheduleList.ScheduleListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View rootView = findViewById(R.id.fragmentContainer);
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (view, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemBars()
+            );
+
+            view.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
+
+            return windowInsets;
+        });
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragmentContainer, new ScheduleListFragment())
+                    .replace(
+                            R.id.fragmentContainer,
+                            new ScheduleListFragment()
+                    )
                     .commit();
         }
     }
