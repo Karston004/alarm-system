c2bddbac98ead9b0e8b96ac5d779352005e6c723
Joe Smith <004jsmith@gmail.com>
2026-08-20 17:51:11 +0100

    Build(Proto): proto now generates nanopb files (cpp proto files for emmbedded systems)

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -26,12 +26,40 @@ protobuf {
         create("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.62.2"
         }
+
+        create("nanopb") {
+            val nanopbPlugin =
+                System.getenv("NANOPB_PLUGIN")
+                    ?: throw GradleException(
+                        """
+                    NANOPB_PLUGIN is not set.
+
+                    Install nanopb:
+                        py -m pip install nanopb==0.4.9.1
+
+                    Then set NANOPB_PLUGIN to the protoc-gen-nanopb executable.
+                    """.trimIndent()
+                    )
+
+            path = nanopbPlugin
+        }
     }
 
     generateProtoTasks {
+
+        // Keep gRPC generation for all proto tasks.
         all().forEach { task ->
             task.plugins {
                 create("grpc")
+            }
+        }
+
+        // Generate Nanopb C output for the main proto source set.
+        ofSourceSet("main").forEach { task ->
+            task.plugins {
+                create("nanopb") {
+                    outputSubDir = "nanopb"
+                }
             }
         }
     }
